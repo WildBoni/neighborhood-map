65fc15826d0a4e65dea83d05c451b76667ca09b3
Davide Bonifacio <web@davidebonifacio.it>
2016-10-05 19:22:27 +0200

    Feature: APImayhem retrieves data from Wikipedia

--- APImayhem/js/script.js
+++ APImayhem/js/script.js
@@ -41,6 +41,25 @@ function loadData() {
     });
 
     //Wikipedia article
+    var wikiRequestTimeout = setTimeout(function(){
+        $wikiElem.text("Failed to get Wikipedia resources");
+    }, 8000);
+
+    var wikiURL = 'https://en.wikipedia.org/w/api.php?action=opensearch&search='
+    + cityStr + '&format=json&callback=wikiCallback'
+
+    $.ajax( {
+        url: wikiURL,
+        dataType: 'jsonp',
+    }).done(function( response ) {
+        var articleList = response[1];
+        for (var i = 0; i < articleList.length; i++) {
+            articleStr = articleList[i]
+            var url = 'http://en.wikipedia.org/wiki/' + articleStr;
+            $wikiElem.append('<li><a href="' + url + '">' + articleStr + '</a></li>');
+        };
+        clearTimeout(wikiRequestTimeout);
+    });
 
     return false;
 };
