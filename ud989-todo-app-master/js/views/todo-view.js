d3ba5c33588c1c592eb6b5f243b5de04f1f87485
Davide Bonifacio <web@davidebonifacio.it>
2016-10-11 23:18:30 +0200

    Feature: todo app highlight priority todos

--- ud989-todo-app-master/js/views/todo-view.js
+++ ud989-todo-app-master/js/views/todo-view.js
@@ -21,6 +21,7 @@ var app = app || {};
 			'dblclick label': 'edit',
 			'click .destroy': 'clear',
 			'click .edit-btn': 'edit',
+			'click .priority-btn': 'togglePriority',
 			'keypress .edit': 'updateOnEnter',
 			'keydown .edit': 'revertOnEscape',
 			'blur .edit': 'close'
@@ -51,6 +52,7 @@ var app = app || {};
 
 			this.$el.html(this.template(this.model.toJSON()));
 			this.$el.toggleClass('completed', this.model.get('completed'));
+			this.$el.toggleClass('priority', this.model.get('highlighted'));
 			this.toggleVisible();
 			this.$input = this.$('.edit');
 			return this;
@@ -69,6 +71,10 @@ var app = app || {};
 		// Toggle the `"completed"` state of the model.
 		toggleCompleted: function () {
 			this.model.toggle();
+		},
+
+		togglePriority: function () {
+			this.model.highlight();
 		},
 
 		// Switch this view into `"editing"` mode, displaying the input field.
